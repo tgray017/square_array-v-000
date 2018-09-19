70e92741107f8a14a7b6f78005c28f2c73f4b5e6
tgray017 <tgray017@gmail.com>
2018-09-19 23:16:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,9 @@ def square_array(array)
   array.each {|x| square_array.push(x**2)}
   square_array
 end
+
+def square_array(array)
+  square_array = []
+  array.collect {|x| square_array.push(x**2)}
+  square_array
+end
